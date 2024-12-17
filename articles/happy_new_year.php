1d03ef3b17e71a9793ce3de172d1dd3e06a5d6f9
BorisiukIvan <121752518+BorisiukIvan@users.noreply.github.com>
2024-12-17 19:20:51 +0100

    Add files via upload

---
+++ articles/happy_new_year.php
@@ -0,0 +1,272 @@
+<html>
+    <head>
+        <meta charset='utf-8'>
+        <title>Итоги 2022 года</title>
+        <style>
+            a {
+                color: blue;
+                text-decoration: none;
+            }
+            #canvas {
+                border: 1px solid black;
+            }
+        </style>
+    </head>
+    <body>
+    <center><h1>Итоги 2022 года</h1></center>
+    <h2>Содержание</h2>
+    <ul>
+        <li><a href='#begin'>История появления сайта</a></li>
+        <li><a href='#compare'>Сравнение сайта сейчас и тогда</a></li>
+        <li><a href='#my_methods'>Статистика по сайту</a></li>
+        <li><a href='#about'>Итоги</a></li>
+        <li><a href='#conclusion'>Куда двигаться дальше?</a></li>
+        <li><a href='/forum/33POSTS'>Обсуждение</a></li>
+    </ul>
+    <h2 id='begin'>История появления сайта</h2>
+    <p>Этот сайт был создан <i>11 сентября 2022 года</i>. Тогда я думал о том, чтобы сделать настоящее общество Антихакеров. Я надеялся, что мы будем работать по-настоящему, то есть узнавать и ликвидировать фишинговые сайты, узнавать и ликвидировать читеров. Ведь мегароботвский АК никак не может похвастаться этим. И я допустил ошибку. Я мечтал о Антихакерах, но <b>я за эти месяцы даже не пытался ничего сделать</b>! Теперь-то я понимаю, что надо было написать программу (я даже теперь знаю, как это сделать), которая будет заниматся Антихакерством автоматически - сразу была бы популярность.</p>
+    <p>Где-то сайт оправдал мои ожидания, где-то нет. Однако минусы сайта в том, что здесь очень мало человек активны. Слишком мало. Но с другой стороны, согласитесь, здесь очень хорошо!</p>
+    <h2 id='compare'>Сравнение сайта сейчас и тогда</h2>
+    <p>Начиная где-то с 12 сентября, все запросы, кроме тех что с моего IP, начали записыватся. Конечно, вначале был какой-то всплеск активности (так всегда), но затем частота запросов уменьшилась. Не знаю, помнит ли кто-то старый сайт. Думаю, только самые старые олды - <a href='/@/darkshark'>@darkshark</a>, зарегистрированная 15 сентября, и <a href='/@/not_found/'>@not_found</a>, зарегистрированный 19-го еще видели тот сайт, который был в то время. Напоминаю, что же там было. Не было ни новостей, ни чата, ни блога, ни шахмат, ни конечно примеров. Личных сообщений и лучших игроков, естественно, не было тоже.</p>
+    <p>Вообще я думаю, что если сравнить старый и новый сайты, то выиграет, конечно же новый. Однако у него один существенный недостаток: нету того, что написано на главной странице. Никаких ПО, никаких баз у нас пока что нету. Еще многие говорят, что у сайта "плохое оформление", однако я с ними не согласен. А что вы думаете по этому поводу?</p>
+    <h2 id='my_methods'>Статистика по сайту</h2>
+    <p>А вот статистика по сайту. Смотрите, и судите сами: растет сайт, или нет, хороший трафик, или нет:</p>
+    <center><canvas width="500" height="500" id="canvas"></canvas></center>
+    <p>Если что, это означает, что в сентябре было 945 просмотров сайта, в октябре 2707, в ноябре 262 и в декабре 3689 (Я исключил запросы ботов-поисковиков и вредоносные запросы). Если вас интересует, почему было так мало запросов в ноябре, то это потому что у меня тогда не было интернета на ноутбуке и я не мог редактировать свой сайт.</p>
+    <p>За время существования сайта на него были 2 атаки. Первая атака была от ботов - они пытались спамить на сайте и взламывать пользователей. Атака была недолгой и я быстро ее прекратил. Вторая атака была атакой на чат, но была дольше. Вот круговая диаграмма, показывающяя, какой процент запросов вредоносный, а какой - нет:</p>
+    <center>
+        <canvas id='myCanvas'></canvas>
+        <br>
+        <table style='width: 60%'>
+        <tr>
+        <td style='width: 19%'><img src='https://phonoteka.org/uploads/posts/2022-02/thumbs/1644446541_1-phonoteka-org-p-krasivii-zheltii-fon-odnotonnii-1.jpg' style='width: 12px; height: 12px'> Люди</td>
+        <td style='width: 19%'><img src='https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/Red_flag.svg/2560px-Red_flag.svg.png' style='width: 12px; height: 12px'> DDoS-атаки</td>
+        <td style='width: 19%'><img src='https://www.macmillandictionary.com/external/slideshow/full/Royal%20Blue_full.png' style='width: 12px; height: 12px'> Боты-поисковики</td>
+        </tr>
+        </table>
+    </center>
+    <p>Как мы видим, DDoS-атаки "cъедают" значительную часть трафика. Однако у меня также есть все IP, причастные к этим атакам. Всего на мой сайт заходило 682 разных IP, из них 232 это боты-поисковики плюс DDoS атаки. Людей на мой сайт заходило 450, при этом несколько IP одинаковых.</p>
+    <p>Насчет страниц, наиболее посещаемыми из них оказались <a href='/math.php'>страница для решения примеров</a> (893 запроса), <a href='/'>главная страница</a> (775 запросов) и <a href='/login.php'>страница входа</a> (635). Понятно, что DDoS запросы и ботов я не учитывал. А теперь переходим к итогам!</p>
+    <h2 id='about'>Итоги</h2>
+    <p>Какие же выводы надо сделать из этой статистики?</p>
+    <p>Как видно по графику, сайт потихоньку развивается, однако не очень быстро (нет, я не прошу вас его ддосить!). Как это ни странно, но самыми успешными страницами оказались именно не антихакерские. Это показывает, что Антихакеры - это не самая выгодная отрасль, но всё-таки очень полезная, если ей правильно руководить. Дальше. Хорошо было бы рекламировать сайт, потому что активных людей (тех, которые постоянно приходят снова и снова) на самом деле только 4 (поисковики не считаются). И то, без меня сайт совсем пропадёт, что красноречиво говорят запросы за ноябрь. Так что, неплохо было бы сделать новые фичи, написать программы.. Но я начал запутыватся в своих обещаниях, поэтому не всё сразу.</p>
+    <h2 id='conclusion'>Куда двигаться дальше?</h2>
+    <p>Возникает вопрос : что же делать дальше? Какие возможности сделать, а может что-то надо и убрать. В планах у меня примерно такое:</p>
+    <p>В прошлом (11 декабря), я <a href='new11122022.php'>обещал</a> сделать шахматного бота, несколько онлайн-игр, ссылки на форуме и упоминания, более продвинутую проверку пароля и ЛС. Я выполнил пока только последних два пункта. Насчет "Нескольких онлайн-игр" я передумал, так как они никакой пользы не несут. В общем, в скором времени я добавлю, пожалуй, только упоминания. А также я собираюсь разрабатывать только API (типо как у личесса). Вот список, того что я буду пытатся сделать.</p>
+    <ul>
+    <li>Упоминания на форуме</li>
+    <li>API</li>
+    <li>Проверка реакции (идея <a href='/@/Maruuulex'>@Maruuulex</a>)</li>
+    <li>Повысить активность сайта</li>
+    <li>Почистить ненужные файлы</li>
+    <li>Выложить код сайта в открытый доступ, чтобы его можно было клонировать</li>
+    </ul>
+    <p>Пока больше идей нет, но они скоро будут. А какие идеи у вас? Пишите в комментариях:</p>
+    <button onclick='window.open("/forum/33POSTS", "_self")'>Обсудить статью на форуме</button>
+    <script>
+       let canvas = document.getElementById('canvas'); 
+ 
+       // Указываем элемент для 2D рисования
+       let ctx2 = canvas.getContext('2d');
+       ctx2.fillStyle = "black"; // Задаём чёрный цвет для линий 
+       ctx2.lineWidth = 2.0; // Ширина линии
+       ctx2.beginPath(); // Запускает путь
+       ctx2.moveTo(30, 10); // Указываем начальный путь
+       ctx2.lineTo(30, 460); // Перемещаем указатель
+       ctx2.lineTo(500, 460); // Ещё раз перемешаем указатель
+       ctx2.stroke(); // Делаем контур
+       // Цвет для рисования
+       ctx2.fillStyle = "black";
+       // Цикл для отображения значений по Y 
+       for (let i = 0; i < 6; i++) { 
+            ctx2.fillText((5 - i) * 700 + "", 4, i * 80 + 60); 
+            ctx2.beginPath(); 
+            ctx2.moveTo(25, i * 80 + 60); 
+            ctx2.lineTo(30, i * 80 + 60); 
+            ctx2.stroke(); 
+       }; 
+ 
+       // Массив с меткам месяцев
+       let labels = ["Сентябрь", "Октябрь", "Ноябрь", "Декабрь"]; 
+ 
+       // Выводим меток
+       for (var i=0; i<4; i++) { 
+            ctx2.fillText(labels[i], 50+ i*100, 475); 
+       }
+       let data = [ 945, 2707, 262, 3689 ]; 
+ 
+       // Назначаем зелёный цвет для графика
+       ctx2.fillStyle = "green"; 
+       // Цикл для от рисовки графиков
+       for(var i=0; i<data.length; i++) { 
+          var dp = data[i]; 
+          ctx2.fillRect(40 + i*100, 460-dp/9 , 50, dp/9); 
+       }
+    </script>
+    <script>
+    var myCanvas = document.getElementById("myCanvas");
+
+    myCanvas.width = 300;
+
+    myCanvas.height = 300;
+
+    let ctx = myCanvas.getContext("2d");
+    function drawLine(ctx, startX, startY, endX, endY){
+
+    ctx.beginPath();
+
+	ctx.moveTo(startX,startY);
+
+	ctx.lineTo(endX,endY);
+
+	ctx.stroke();
+
+    }
+    function drawArc(ctx, centerX, centerY, radius, startAngle, endAngle){
+
+    ctx.beginPath();
+
+	ctx.arc(centerX, centerY, radius, startAngle, endAngle);
+
+	ctx.stroke();
+
+    }
+    function drawPieSlice(ctx,centerX, centerY, radius, startAngle, endAngle, color ){
+
+    ctx.fillStyle = color;
+
+	ctx.beginPath();
+
+	ctx.moveTo(centerX,centerY);
+
+	ctx.arc(centerX, centerY, radius, startAngle, endAngle);
+
+	ctx.closePath();
+
+	ctx.fill();
+
+    }
+    var myVinyls = {
+
+    "Human": 64.5,
+
+	"DDoS": 31.8,
+
+	"Bots": 2.7
+
+    };
+    var Piechart = function(options){
+
+    this.options = options;
+
+	this.canvas = options.canvas;
+
+	this.ctx = this.canvas.getContext("2d");
+
+	this.colors = options.colors;
+
+	this.draw = function(){
+
+		var total_value = 0;
+
+		var color_index = 0;
+
+		for (var categ in this.options.data){
+
+			var val = this.options.data[categ];
+
+			total_value += val;
+
+		}
+
+		var start_angle = 0;
+
+		for (categ in this.options.data){
+
+			val = this.options.data[categ];
+
+			var slice_angle = 2 * Math.PI * val / total_value;
+
+			drawPieSlice(
+
+				this.ctx,
+
+				this.canvas.width/2,
+
+				this.canvas.height/2,
+
+				Math.min(this.canvas.width/2,this.canvas.height/2),
+
+				start_angle,
+
+				start_angle+slice_angle,
+
+				this.colors[color_index%this.colors.length]
+
+			);
+
+			start_angle += slice_angle;
+
+			color_index++;
+
+		}
+                start_angle = 0;
+
+		for (categ in this.options.data){
+
+			val = this.options.data[categ];
+
+			slice_angle = 2 * Math.PI * val / total_value;
+
+			var pieRadius = Math.min(this.canvas.width/2,this.canvas.height/2);
+
+			var labelX = this.canvas.width/2 + (pieRadius / 2) * Math.cos(start_angle + slice_angle/2);
+
+			var labelY = this.canvas.height/2 + (pieRadius / 2) * Math.sin(start_angle + slice_angle/2);
+
+			if (this.options.doughnutHoleSize){
+
+				var offset = (pieRadius * this.options.doughnutHoleSize ) / 2;
+
+				labelX = this.canvas.width/2 + (offset + pieRadius / 2) * Math.cos(start_angle + slice_angle/2);
+
+				labelY = this.canvas.height/2 + (offset + pieRadius / 2) * Math.sin(start_angle + slice_angle/2);				
+
+			}
+
+			var labelText = Math.round(100 * val / total_value);
+
+			this.ctx.fillStyle = "white";
+
+			this.ctx.font = "bold 20px Arial";
+
+			this.ctx.fillText(labelText+"%", labelX,labelY);
+
+			start_angle += slice_angle;
+
+		}
+
+
+	}
+
+    }
+    var myPiechart = new Piechart(
+
+    {
+
+		canvas:myCanvas,
+
+		data:myVinyls,
+
+		colors:["yellow", "red", "blue"]
+
+	}
+
+    );
+
+    myPiechart.draw();
+    
+
+    </script>
+    </body>
+</html>
